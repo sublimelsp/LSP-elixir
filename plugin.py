e0869cbf3bbdda1f8f7c92f5e978fd4aedbdd72b
Po Chen <chenpaul914@gmail.com>
2021-02-18 13:10:14 +1100

    use lsp_utils

--- plugin.py
+++ plugin.py
@@ -1,149 +1,34 @@
-import hashlib
-import os
-import shutil
 import sublime
 
-from LSP.plugin.core import logging
-from LSP.plugin.core.handlers import LanguageHandler
-from LSP.plugin.core.settings import read_client_config
-from sublime_lib import ActivityIndicator
-from threading import Thread
-from urllib.request import urlretrieve
-from zipfile import ZipFile
+from lsp_utils import GenericClientHandler, ServerResourceInterface
+from LSP.plugin.core.typing import Optional
+from LSP.plugin import DottedDict
+from LSP.plugin.core.typing import List
 
-SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.6.4/elixir-ls.zip"
-SERVER_HASH = "4c1be629ddd98908e4c0e46284f41afc27c5cde4eded946ed343ea5eb7f5e813"
+from .server_zip_resource import ServerZipResource
+
+SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.6.4/elixir-ls-1.11.zip"
+SERVER_HASH = "30df49d0c7c12717814a3b03d4b8064197820a10beac138ad6d63d28f5295a4f"
 SERVER_VERSION = "0.6.4"
 
-IS_WINDOWS = sublime.platform() == "windows"
-
-DEAFULT_SETTINGS = {
-    'env': {},
-    'experimental_capabilities': {},
-    'languages': [],
-    'initializationOptions': {},
-    'settings': {},
-}
+SERVER_EXECUTABLES = ["language_server.sh", "launch.sh"]
+BINARY_PATH = "language_server.bat" if sublime.platform() == 'windows' else "language_server.sh"
 
 
-def log_debug(msg):
-    logging.debug("lsp-elixir: {}".format(msg))
+class LspElixirPlugin(GenericClientHandler):
+    package_name = __package__
+    __server = None
 
-
-def is_elixir_installed():
-    return shutil.which('elixir') is not None
-
-
-def get_cache_path():
-    cache_path = os.path.join(sublime.cache_path(), __package__)
-    os.makedirs(cache_path, exist_ok=True)
-    return cache_path
-
-
-def get_server_dir(version):
-    return os.path.join(get_cache_path(), "server", version)
-
-
-def get_server_exec():
-    """Returns path to the server executable (it may not exist)."""
-    exe = "language_server.bat" if IS_WINDOWS else "language_server.sh"
-    return os.path.join(get_server_dir(SERVER_VERSION), exe)
-
-
-def is_valid_hash(path, expected):
-    with open(path, "rb") as f:
-        calculated = hashlib.sha256(f.read()).hexdigest()
-        return calculated == expected
-
-
-def unpack_server(zip_file):
-    if not is_valid_hash(zip_file, SERVER_HASH):
-        log_debug("Invalid hash, aborting")
-        return
-
-    target_dir = get_server_dir(SERVER_VERSION)
-    os.makedirs(target_dir, exist_ok=True)
-    log_debug("Unpacking server to: {}".format(target_dir))
-
-    with ZipFile(zip_file, "r") as f:
-        f.extractall(target_dir)
-
-    # ZipFile removes permissions, make server executable
-    if not IS_WINDOWS:
-        os.chmod(os.path.join(target_dir, "language_server.sh"), 0o755)
-        os.chmod(os.path.join(target_dir, "launch.sh"), 0o755)
-
-    log_debug("Server installed")
-
-
-def download_server():
-    log_debug("Downloading server from {}".format(SERVER_URL))
-    target = sublime.active_window()
-    label = "Downloading Elixir language server binary"
-
-    with ActivityIndicator(target, label):
-        try:
-            tmp_file, _ = urlretrieve(SERVER_URL)
-            unpack_server(tmp_file)
-            os.unlink(tmp_file)
-        except Exception as ex:
-            log_debug("Failed downloading server: {}".format(ex))
-
-
-def download_server_async():
-    thread = Thread(target=download_server)
-    thread.start()
-
-
-def delete_server():
-    target_dir = get_server_dir(SERVER_VERSION)
-    log_debug("Deleting server from {}".format(target_dir))
-    shutil.rmtree(target_dir, ignore_errors=True)
-
-
-def is_server_downloaded():
-    return os.path.exists(get_server_exec())
-
-
-class LspElixirPlugin(LanguageHandler):
-    @property
-    def name(self):
-        return __package__.lower()
-
-    @property
-    def config(self):
-        config = {
-            "enabled": True,
-            "command": [get_server_exec()],
-        }
-
-        filename = '{}.sublime-settings'.format(__package__)
-        settings = sublime.load_settings(filename)
-        for key, default in DEAFULT_SETTINGS.items():
-            config[key] = settings.get(key, default)
-
-        return read_client_config(self.name, config)
-
-    def on_start(self, window):
-        if not is_elixir_installed():
-            sublime.status_message(
-                "Please install Elixir for LSP-elixir plugin to work.")
-            return False
-
-        if not is_server_downloaded():
-            sublime.status_message(
-                "Elixir Language Server not yet downloaded.")
-            return False
-
+    @classmethod
+    def manages_server(cls) -> bool:
         return True
 
-
-def plugin_loaded():
-    if not is_server_downloaded():
-        download_server_async()
-
-
-def plugin_unloaded():
-    """Called when the plugin is disabled or removed."""
-    if is_server_downloaded():
-        delete_server()
+    @classmethod
+    def get_server(cls) -> Optional[ServerResourceInterface]:
+        if not cls.__server:
+            cls.__server = ServerZipResource(cls.package_name,
+                                             BINARY_PATH,
+                                             SERVER_URL,
+                                             SERVER_VERSION,
+                                             executables=SERVER_EXECUTABLES)
+        return cls.__server
---
+++ server_zip_resource.py
@@ -0,0 +1,97 @@
+import hashlib
+import os
+import shutil
+import sublime
+
+from zipfile import ZipFile
+from urllib.request import urlretrieve
+
+from lsp_utils import ServerResourceInterface, ServerStatus
+from sublime_lib import ActivityIndicator
+from LSP.plugin.core.typing import List
+
+__all__ = ['ServerZipResource']
+
+
+class ServerZipResource(ServerResourceInterface):
+    def __init__(self,
+                 package_name: str,
+                 binary_path: str,
+                 asset_url: str,
+                 asset_version: str,
+                 *,
+                 asset_hash: str = None,
+                 executables: List[str] = []) -> None:
+        self._package_name = package_name
+        self._binary_path = binary_path
+        self._url = asset_url
+        self._version = asset_version
+        self._status = ServerStatus.UNINITIALIZED
+
+        self._hash = asset_hash
+        self._executables = executables
+
+    def get_cache_path(self) -> str:
+        cache_path = os.path.join(sublime.cache_path(), __package__)
+        os.makedirs(cache_path, exist_ok=True)
+        return cache_path
+
+    def get_server_dir(self) -> str:
+        return os.path.join(self.get_cache_path(), "server", self._version)
+
+    def get_server_exec(self) -> str:
+        return os.path.join(self.get_server_dir(), self._binary_path)
+
+    def is_server_downloaded(self):
+        return os.path.exists(self.get_server_exec())
+
+    def is_valid_hash(self, path, expected) -> bool:
+        if not self._hash:
+            return True
+
+        with open(path, "rb") as f:
+            calculated = hashlib.sha256(f.read()).hexdigest()
+            return calculated == expected
+
+    def unpack_server(self, zip_file) -> None:
+        if not self.is_valid_hash(zip_file, self._hash):
+            return
+
+        target_dir = self.get_server_dir()
+        os.makedirs(target_dir, exist_ok=True)
+
+        with ZipFile(zip_file, "r") as f:
+            f.extractall(target_dir)
+
+        # ZipFile removes permissions, make server executable
+        if self._executables and not sublime.platform() == 'windows':
+            for executable in self._executables:
+                os.chmod(os.path.join(target_dir, executable), 0o755)
+
+    def download_server(self) -> None:
+        target = sublime.active_window()
+        label = "Downloading zip file..."
+
+        with ActivityIndicator(target, label):
+            tmp_file, _ = urlretrieve(self._url)
+            self.unpack_server(tmp_file)
+            os.unlink(tmp_file)
+
+    # ServerResourceInterface
+
+    def get_status(self) -> int:
+        return self._status
+
+    def needs_installation(self) -> bool:
+        if self.is_server_downloaded():
+            self._status = ServerStatus.READY
+            return False
+        return True
+
+    def install_or_update(self) -> None:
+        self.download_server()
+        self._status = ServerStatus.READY
+
+    @property
+    def binary_path(self) -> str:
+        return self.get_server_exec()
