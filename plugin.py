349ccb5a174254dbd825ff04a83539f2f7f9731a
Po Chen <chenpaul914@gmail.com>
2023-06-20 21:33:54 +1000

    V0.15.0 (#40)
    * update script to cope with elixir-ls release changes
    
    * bump to 0.15.0

--- plugin.py
+++ plugin.py
@@ -5,12 +5,14 @@ from LSP.plugin.core.typing import Optional
 
 from .server_zip_resource import ServerZipResource
 
-SERVER_VERSION = "0.14.6"
-SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.14.6/elixir-ls.zip"
-SERVER_SHA256 = "00c954d8ced5a25c6a90b41794d607ee2018134f4ff1d2987812f3b8e6d26c22"
+SERVER_VERSION = "0.15.0"
+SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.15.0/elixir-ls-v0.15.0.zip"
+SERVER_SHA256 = "d388294c509c4e1fee023aa3723f241d453858910b28670c81fbd337f91d1ba9"
 
 SERVER_EXECUTABLES = ["language_server.sh", "launch.sh"]
-BINARY_PATH = "language_server.bat" if sublime.platform() == 'windows' else "language_server.sh"
+BINARY_PATH = (
+    "language_server.bat" if sublime.platform() == "windows" else "language_server.sh"
+)
 
 
 def plugin_loaded() -> None:
@@ -36,11 +38,13 @@ class LspElixirPlugin(GenericClientHandler):
     @classmethod
     def get_server(cls) -> Optional[ServerResourceInterface]:
         if not cls.__server:
-            cls.__server = ServerZipResource(cls.storage_path(),
-                                             cls.package_name,
-                                             BINARY_PATH,
-                                             SERVER_URL,
-                                             SERVER_VERSION,
-                                             asset_hash=SERVER_SHA256,
-                                             executables=SERVER_EXECUTABLES)
+            cls.__server = ServerZipResource(
+                cls.storage_path(),
+                cls.package_name,
+                BINARY_PATH,
+                SERVER_URL,
+                SERVER_VERSION,
+                asset_hash=SERVER_SHA256,
+                executables=SERVER_EXECUTABLES,
+            )
         return cls.__server
