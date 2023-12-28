66fd1716c70b8e04662afe4653a3f3f8f2075776
Po Chen <princemaple@users.noreply.github.com>
2023-12-28 01:07:01 +0000

    Bump elixir-ls to 0.18.1

--- plugin.py
+++ plugin.py
@@ -5,9 +5,9 @@ from LSP.plugin.core.typing import Optional
 
 from .server_zip_resource import ServerZipResource
 
-SERVER_VERSION = "0.18.0"
-SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.18.0/elixir-ls-v0.18.0.zip"
-SERVER_SHA256 = "fe4dd788e57c8fe0c2852eb14625ea0f90a7d10e987af35512b6e4788c8d77d4"
+SERVER_VERSION = "0.18.1"
+SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.18.1/elixir-ls-v0.18.1.zip"
+SERVER_SHA256 = "e7dc292ceb0ac9fb456d98fa306ef3b2d1b8785d0e9a1c0b5c7c7e3a6c724963"
 
 SERVER_EXECUTABLES = ["language_server.sh", "launch.sh"]
 BINARY_PATH = (
