a4111d4b7765b63b057121cdf57249d5299f149c
Udo <git@udo.codes>
2024-10-15 14:03:26 +0200

    Bump elixir-ls to 0.24.1

--- plugin.py
+++ plugin.py
@@ -5,9 +5,9 @@ from LSP.plugin.core.typing import Optional
 
 from .server_zip_resource import ServerZipResource
 
-SERVER_VERSION = "0.23.0"
-SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.23.0/elixir-ls-v0.23.0.zip"
-SERVER_SHA256 = "d610b67d1172cf2183709749cfe2f2caf5e640e241bb2eeba3593b4d0fb97ff1"
+SERVER_VERSION = "0.24.1"
+SERVER_URL = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.24.1/elixir-ls-v0.24.1.zip"
+SERVER_SHA256 = "99595d3fa5bec269057632de8160077e313f10369314b04017cc93c7f80f39a7"
 
 SERVER_EXECUTABLES = ["language_server.sh", "launch.sh"]
 BINARY_PATH = (
